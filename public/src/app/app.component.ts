f33ea0b02fe730669528d8fd596faa1532540f81
Bart Blokhuis <info@bartblokhuis.com>
2021-12-16 16:11:07 +0100

    Fixed AOS error in server console

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { Meta, Title } from '@angular/platform-browser';
 import { GeneralSettings } from './data/GeneralSettings';
 import { SeoSettings } from './data/SeoSettings';
@@ -14,36 +14,33 @@ declare var AOS: any;
 })
 export class AppComponent implements OnInit {
   finishedLoading: boolean = false;
-
   seoSettings: SeoSettings | null = null;
+
+  @HostListener("window:load", []) onWindowLoad() {
+    if(AOS) AOS.init();
+  }
 
   constructor(private settingsService: SettingsService, private title: Title, private meta: Meta) { }
 
   ngOnInit(): void {
-    // this.settingsService.get<SeoSettings>('SeoSettings').toPromise().then(result => {
-    //   this.seoSettings = result;
+    this.settingsService.get<SeoSettings>('SeoSettings').toPromise().then(result => {
+      this.seoSettings = result;
 
-    //  this.title.setTitle(this.seoSettings.title);
-    //  this.meta.addTag({ name: 'description', content: this.seoSettings.defaultMetaDescription});
+     this.title.setTitle(this.seoSettings.title);
+     this.meta.addTag({ name: 'description', content: this.seoSettings.defaultMetaDescription});
      
-    //  if(this.seoSettings.useTwitterMetaTags){
-    //   this.meta.addTag({ name: 'twitter:title', content: this.seoSettings.title});
-    //   this.meta.addTag({ name: 'twitter:description', content: this.seoSettings.defaultMetaDescription});
+     if(this.seoSettings.useTwitterMetaTags){
+      this.meta.addTag({ name: 'twitter:title', content: this.seoSettings.title});
+      this.meta.addTag({ name: 'twitter:description', content: this.seoSettings.defaultMetaDescription});
 
-    //  }
-    //  if(this.seoSettings.useOpenGraphMetaTags){
-    //   this.meta.addTag({ name: 'og:title', content: this.seoSettings.title});
-    //   this.meta.addTag({ name: 'og:description', content: this.seoSettings.defaultMetaDescription});
-    //  }
+     }
+     if(this.seoSettings.useOpenGraphMetaTags){
+      this.meta.addTag({ name: 'og:title', content: this.seoSettings.title});
+      this.meta.addTag({ name: 'og:description', content: this.seoSettings.defaultMetaDescription});
+     }
 
-    //   this.finishedLoading = true;
-    // })
-
-    this.settingsService.get<SeoSettings>('SeoSettings').subscribe((result) => {
-      this.title.setTitle(result.title);
+      this.finishedLoading = true;
     });
-
-    if(AOS) AOS.init();
   }
 
 }
