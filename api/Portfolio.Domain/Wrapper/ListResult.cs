591053293b634402b611ed432f2c4173518ee2e9
Bart Blokhuis <info@bartblokhuis.com>
2021-12-20 15:04:17 +0100

    Created the generic Result class

---
+++ api/Portfolio.Domain/Wrapper/ListResult.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+
+namespace Portfolio.Domain.Wrapper;
+
+public class ListResult<T> : Result
+{
+    public ListResult(List<T> data)
+    {
+        Data = data;
+    }
+
+    public List<T> Data { get; set; }
+
+    internal ListResult(bool succeeded, List<T> data = default, List<string> messages = null)
+    {
+        Data = data;
+        Succeeded = succeeded;
+    }
+
+    public static ListResult<T> Failure(List<string> messages)
+    {
+        return new(false, default, messages);
+    }
+
+    public static ListResult<T> Success(List<T> data)
+    {
+        return new(true, data, null);
+    }
+
+    public static ListResult<T> Success(List<T> data, string message)
+    {
+        return new(true, data, new List<string> { message });
+    }
+}
