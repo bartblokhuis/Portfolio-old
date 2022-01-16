fce43c79ca17f3244128fd3c9c3549bd7216728b
Bart Blokhuis <info@bartblokhuis.com>
2022-01-16 18:11:57 +0100

    Created the dashboard messages component and made it load the 5 latest messages

--- admin/src/app/features/dashboard/dashboard.module.ts
+++ admin/src/app/features/dashboard/dashboard.module.ts
@@ -3,15 +3,19 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { DashBoardRoutes } from './dashboard.routes';
 import { RouterModule } from '@angular/router';
+import { DashboardMessagesComponent } from './dashboard-messages/dashboard-messages.component';
+import { SharedComponentsModule } from 'src/app/components/shared/shared-components.module';
 
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    DashboardMessagesComponent
   ],
   imports: [
     CommonModule,
+    SharedComponentsModule,
     RouterModule.forChild(DashBoardRoutes),
   ]
 })
