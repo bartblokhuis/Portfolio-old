e3c4e694d13587046743682d593d6c7d55dff72c
Bart Blokhuis <info@bartblokhuis.com>
2022-01-17 21:32:24 +0100

    Created the blog post comments entity and migration

---
+++ api/Portfolio.Domain/Models/Comment.cs
@@ -0,0 +1,29 @@
+﻿using Portfolio.Domain.Models.Common;
+using System.Collections.Generic;
+
+namespace Portfolio.Domain.Models;
+
+public class Comment : BaseEntity
+{
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string Content { get; set; }
+
+    public int Thumbsup { get; set; }
+
+    public int Thumbsdown { get; set; }
+
+    public bool IsAuthor { get; set; }
+
+    public BlogPost BlogPost { get; set; }
+
+    public int BlogPostId { get; set; }
+
+    public Comment ParentComment { get; set; }
+
+    public int ParentCommentId { get; set; }
+
+    public ICollection<Comment> Comments { get; set; }
+}
