f91b4e41d6afdb859c56efc88dee01062a2150e5
Bart Blokhuis <info@bartblokhuis.com>
2022-01-19 14:15:46 +0100

    Created the Url model and updated the project class
    Removed the DemoUrl and GithubUrl properties from the Project class and added the new URLs class

---
+++ api/Portfolio.Domain/Models/Url.cs
@@ -0,0 +1,12 @@
+﻿namespace Portfolio.Domain.Models;
+public class Url
+{
+    #region Properties
+
+    public string FullUrl  { get; set; }
+
+    public string FriendlyName { get; set; }
+
+    #endregion
+}
+
