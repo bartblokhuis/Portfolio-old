d72eacdc448c74f3b30765cdd6a8cb7d47d6be15
Bart Blokhuis <info@bartblokhuis.com>
2022-01-19 13:45:23 +0100

    Created the system controller with a method for clearing the cache

---
+++ api/Portfolio.Api/Controllers/SystemController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Core.Caching;
+using Portfolio.Domain.Wrapper;
+using System.Threading.Tasks;
+
+namespace Portfolio.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class SystemController : ControllerBase
+{
+    #region Fields
+
+    private readonly IStaticCacheManager _cacheManager;
+
+    #endregion
+
+    #region Constructor
+
+    public SystemController(IStaticCacheManager cacheManager)
+    {
+        _cacheManager = cacheManager;
+    }
+
+    #endregion
+
+    #region Methods
+
+    [HttpGet("ClearCache")]
+    public async Task<IActionResult> ClearCache()
+    {
+        await _cacheManager.ClearAsync();
+
+        var result = await Result.SuccessAsync("Cleared the system cache");
+        return Ok(result);
+    }
+
+    #endregion
+}
+
