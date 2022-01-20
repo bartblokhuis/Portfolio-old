26b2c23fc3ef7ab74efc3ddcf98584e73563a400
Bart Blokhuis <info@bartblokhuis.com>
2022-01-20 17:26:24 +0100

    Created the popup effect

--- public/src/app/features/landing/projects/list-project/list-project.component.ts
+++ public/src/app/features/landing/projects/list-project/list-project.component.ts
@@ -1,10 +1,11 @@
 import { DOCUMENT } from '@angular/common';
-import { Component, ComponentFactoryResolver, Inject, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, Inject, OnInit, ViewContainerRef } from '@angular/core';
 import { Project } from 'src/app/data/project/Project';
 import { Skill } from 'src/app/data/Skill';
 import { ProjectsService } from 'src/app/services/projects/projects.service';
 import { environment } from 'src/environments/environment';
 import Swal from 'sweetalert2';
+import { ProjectComponent } from '../project/project.component';
 
 @Component({
   selector: 'app-list-project',
@@ -16,15 +17,12 @@ export class ListProjectComponent implements OnInit {
   projects: Project[] | null = null;
   baseUrl: string = environment.baseApiUrl;
 
-  constructor(private projectsService: ProjectsService, @Inject(DOCUMENT) private document: Document, private componentFactoryResolver: ComponentFactoryResolver) { }
+  constructor(private projectsService: ProjectsService, @Inject(DOCUMENT) private document: Document, private viewContainerRef: ViewContainerRef) { }
 
   ngOnInit(): void {
     this.projectsService.get().subscribe((result) => {
       if(result.succeeded) this.projects = result.data;
-    })
-
-    //let childComponent = this.componentFactoryResolver.resolveComponentFactory(ChildComponent);
-    //this.componentRef = this.target.createComponent(childComponent);
+    });
   }
 
   readMore(project: Project) {
@@ -39,13 +37,19 @@ export class ListProjectComponent implements OnInit {
     const modalContent = `<div class="modal-image"></div>
     <div class="modal-title">${project.title}</div> <div class="project-skills">${this.printSkill(project.skills)}</div> <div class="modal-content">${project.description}</div><div class="modal-footer">${buttons}</div>`;
 
-    Swal.fire({
-      title: "",
-      showCancelButton: false,
-      showConfirmButton: false,
-      html: modalContent,
-      heightAuto: false,
-    });
+    // Swal.fire({
+    //   title: "",
+    //   showCancelButton: false,
+    //   showConfirmButton: false,
+    //   html: modalContent,
+    //   heightAuto: false,
+    // });
+
+    var projectComponent = this.viewContainerRef.createComponent(ProjectComponent);
+    projectComponent.instance.project = project;
+    projectComponent.instance.onClose.subscribe(() => {
+      projectComponent.destroy();
+    })
   }
 
   printSkill(skills: Skill[] | undefined): string {
--- public/src/app/features/landing/projects/project/project.component.ts
+++ public/src/app/features/landing/projects/project/project.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, EventEmitter, Inject, Input, OnInit, Output } from '@angular/core';
+import { Project } from 'src/app/data/project/Project';
+import { environment } from 'src/environments/environment';
+
 
 @Component({
   selector: 'app-project',
@@ -7,9 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectComponent implements OnInit {
 
-  constructor() { }
+  @Input() project: Project | null = null;
+  @Output() onClose: EventEmitter<any> = new EventEmitter();
+  baseUrl: string = environment.baseApiUrl;
+
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+
 
   ngOnInit(): void {
+    console.log(this.project)
+
+    
+  }
+
+  close() {
+    var container = this.document.getElementsByClassName('pop-up-container')[0];
+    container.classList.add('close');
+    setTimeout(() => this.onClose.emit(), 300);
   }
 
 }
