4f5132e5be8ff939958587eb2085e6111a920468
Bart Blokhuis <info@bartblokhuis.com>
2022-01-22 19:13:17 +0100

    Created the new setting dto's and added them to the automapper config

---
+++ api/Portfolio.Core/AutoMapper/SettingsProfile.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Portfolio.Domain.Dtos;
+using Portfolio.Domain.Dtos.Settings;
+using Portfolio.Domain.Models.Settings;
+
+namespace Portfolio.Core.AutoMapper;
+
+public class SettingsProfile : Profile
+{
+    #region Constructor
+
+    public SettingsProfile()
+    {
+        CreateMap<EmailSettings, EmailSettingsDto>();
+        CreateMap<EmailSettingsDto, EmailSettings>().ForMember(x => x.Id, options => options.Ignore());
+
+        CreateMap<SeoSettings, SeoSettingsDto>();
+        CreateMap<SeoSettingsDto, SeoSettings>().ForMember(x => x.Id, options => options.Ignore());
+
+        CreateMap<GeneralSettings, GeneralSettingsDto>();
+        CreateMap<GeneralSettingsDto, GeneralSettings>().ForMember(x => x.Id, options => options.Ignore());
+
+        CreateMap<PublicSiteSettingsDto, PublicSiteSettings>().ForMember(x => x.Id, options => options.Ignore()).ReverseMap();
+        CreateMap<BlogSettingsDto, BlogSettings>().ForMember(x => x.Id, options => options.Ignore()).ReverseMap();
+    }
+
+    #endregion
+}
---
+++ api/Portfolio.Domain/Dtos/Settings/BlogSettingsDto.cs
@@ -0,0 +1,16 @@
+﻿namespace Portfolio.Domain.Dtos.Settings;
+
+public class BlogSettingsDto
+{
+    #region Properties
+
+    public bool IsSendEmailOnSubscribing { get; set; }
+
+    public string EmailOnSubscribingTemplate { get; set; }
+
+    public bool IsSendEmailOnPublishing { get; set; }
+
+    public string EmailOnPublishingTemplate { get; set; }
+
+    #endregion
+}
