c6063af1020564aab473115fc7571639a1ad0ca0
Bart Blokhuis <info@bartblokhuis.com>
2022-01-23 18:01:03 +0100

    Cleaned up dashboard blog subscribers

--- angular/projects/admin/src/app/features/dashboard/dashboard-blog-subscribers/dashboard-blog-subscribers.component.ts
+++ angular/projects/admin/src/app/features/dashboard/dashboard-blog-subscribers/dashboard-blog-subscribers.component.ts
@@ -1,12 +1,9 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChartDataset, ChartOptions } from 'chart.js';
+import { ThemeService } from 'ng2-charts';
 import { BlogSubscribersService } from 'projects/shared/src/lib/services/api/blog-subscribers/blog-subscribers.service';
-import { ChartDataset, ChartOptions, Chart, ChartConfiguration, ChartEvent, ChartType, DefaultDataPoint } from 'chart.js';
-import { BaseChartDirective, ThemeService } from 'ng2-charts';
 import { ThemingService } from '../../../services/theming/theming.service';
 
-declare var $: any;
-declare var moment: any;
-declare var Sparkline: any;
 @Component({
   selector: 'app-dashboard-blog-subscribers',
   templateUrl: './dashboard-blog-subscribers.component.html',
@@ -16,7 +13,7 @@ export class DashboardBlogSubscribersComponent implements OnInit {
 
   loaded: boolean = false;
   selectedPeriod = "week";
-  themeColor = '#fff';
+  themeColor:string = "#fff";
 
   lineChartData: ChartDataset[] = [];
   lineChartLabels = [''];
@@ -34,27 +31,6 @@ export class DashboardBlogSubscribersComponent implements OnInit {
       intersect: true
     },
     indexAxis: 'x',
-    scales: {
-      xAxis: {
-        grid: {
-          display: false,
-        },
-        display: true,
-        ticks: {
-          display: true,
-          autoSkip: true,
-          maxTicksLimit: 6,
-          color: this.themeColor
-        }
-      },
-      yAxis: {
-        display: true,
-        min: 0,
-        ticks: {
-          color: this.themeColor
-        }
-      }
-    }
   };
 
   lineChartLegend = false;
@@ -64,13 +40,12 @@ export class DashboardBlogSubscribersComponent implements OnInit {
   constructor(private readonly blogSubscribersService: BlogSubscribersService, private readonly themingService: ThemingService, private themeService: ThemeService) { }
 
   ngOnInit(): void {
-    this.loadChartData();
+    
 
     this.themingService.theme.subscribe((theme) => {
       if(theme === 'dark-mode') this.themeColor = '#fff';
       else this.themeColor = '#000';
 
-      
       this.themeService.setColorschemesOptions({
         scales: {
           xAxis: {
@@ -89,11 +64,14 @@ export class DashboardBlogSubscribersComponent implements OnInit {
             display: true,
             min: 0,
             ticks: {
-              color: this.themeColor
+              color: this.themeColor,
+              precision: 0
             }
           }
         }
       })
+
+      this.loadChartData();
       
     })
   }
@@ -109,10 +87,6 @@ export class DashboardBlogSubscribersComponent implements OnInit {
       if(!result.succeeded) return;
 
       this.lineChartData = [];
-      result.data.forEach(element => {
-       
-        
-      });
       this.lineChartLabels = result.data.map(x => x.date.toString());
       
       const data = result.data.map(x => x.value);
