35b1605e58803ec59b08635bdd3f9b02dac8fbc0
Bart Blokhuis <info@bartblokhuis.com>
2022-01-26 15:09:45 +0100

    Fixes #67

--- api/src/Portfolio.Core/AutoMapper/SettingsProfile.cs
+++ api/src/Portfolio.Core/AutoMapper/SettingsProfile.cs
@@ -23,6 +23,7 @@ public class SettingsProfile : Profile
         CreateMap<PublicSiteSettingsDto, PublicSiteSettings>().ForMember(x => x.Id, options => options.Ignore()).ReverseMap();
         CreateMap<BlogSettingsDto, BlogSettings>().ForMember(x => x.Id, options => options.Ignore()).ReverseMap();
         CreateMap<MessageSettingsDto, MessageSettings>().ForMember(x => x.Id, options => options.Ignore()).ReverseMap();
+        CreateMap<ApiSettingsDto, ApiSettings>().ForMember(x => x.Id, options => options.Ignore()).ReverseMap();
     }
 
     #endregion
