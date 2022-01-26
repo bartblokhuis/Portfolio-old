84df07c76ae26cf8c37e7f9b95b3619738921239
Bart Blokhuis <info@bartblokhuis.com>
2022-01-26 17:20:58 +0100

    Added confirm notifications for the picture components

--- angular/projects/admin/src/app/features/pictures/delete-picture/delete-picture.component.ts
+++ angular/projects/admin/src/app/features/pictures/delete-picture/delete-picture.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
 import { Picture } from 'projects/shared/src/lib/data/common/picture';
 import { PicturesService } from 'projects/shared/src/lib/services/api/pictures/pictures.service';
+import { NotificationService } from '../../../services/notification/notification.service';
 
 @Component({
   selector: 'app-delete-picture',
@@ -13,7 +14,7 @@ export class DeletePictureComponent implements OnInit {
   @Input() picture: Picture = { altAttribute: '', id: 0, mimeType: '', path: '', titleAttribute: ''};
   @Input() modal: NgbModalRef | undefined;
 
-  constructor(private picturesService: PicturesService) { }
+  constructor(private picturesService: PicturesService, private notificationService: NotificationService) { }
 
   ngOnInit(): void {
   }
@@ -23,6 +24,9 @@ export class DeletePictureComponent implements OnInit {
     if(!this.picture.id) return;
 
     this.picturesService.delete(this.picture.id).subscribe((result) => {
+      if(result.succeeded) {
+        this.notificationService.success("Removed the picture");
+      }
       this.modal?.close(result);
     })
   }
