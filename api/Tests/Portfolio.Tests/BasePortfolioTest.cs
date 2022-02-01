5fcda7537a1636f41bd8aadf05e5bb2de97a935d
Bart Blokhuis <info@bartblokhuis.com>
2022-02-01 16:50:23 +0100

    Configured in memory database and made a unit test for the blog service

--- api/Tests/Portfolio.Tests/BasePortfolioTest.cs
+++ api/Tests/Portfolio.Tests/BasePortfolioTest.cs
@@ -1,7 +1,32 @@
-﻿using Microsoft.Extensions.Caching.Memory;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Net.Http.Headers;
+using Moq;
 using Portfolio.Core.Caching;
+using Portfolio.Core.Configuration;
+using Portfolio.Core.Events;
 using Portfolio.Core.Infrastructure;
+using Portfolio.Database;
+using Portfolio.Domain.Models.Blogs;
+using Portfolio.Services.AboutMeServices;
+using Portfolio.Services.Blogs;
+using Portfolio.Services.BlogSubscribers;
+using Portfolio.Services.Comments;
+using Portfolio.Services.Common;
+using Portfolio.Services.Messages;
+using Portfolio.Services.Pictures;
+using Portfolio.Services.Projects;
+using Portfolio.Services.QueuedEmails;
+using Portfolio.Services.Repository;
+using Portfolio.Services.Settings;
+using Portfolio.Services.SkillGroups;
+using Portfolio.Services.Skills;
+using Portfolio.Services.Tasks;
+using Portfolio.Services.Tokens;
+using Portfolio.Services.Urls;
 using System;
 
 namespace Portfolio.Tests;
@@ -20,11 +45,53 @@ public abstract class BasePortfolioTest
         var memoryCache = new MemoryCache(new MemoryCacheOptions());
         var typeFinder = new AppDomainTypeFinder();
 
+        var httpContext = new DefaultHttpContext
+        {
+            Request = { Headers = { { HeaderNames.Host, "127.0.0.1" } } }
+        };
+
+        var httpContextAccessor = new Mock<IHttpContextAccessor>();
+        httpContextAccessor.Setup(p => p.HttpContext).Returns(httpContext);
+
+        services.AddSingleton(httpContextAccessor.Object);
+
         services.AddSingleton<IMemoryCache>(memoryCache);
         services.AddSingleton<IStaticCacheManager, MemoryCacheManager>();
         services.AddSingleton<ILocker, MemoryCacheManager>();
 
+        services.AddDbContext<PortfolioContext>((builder) =>
+        {
+            builder.UseInMemoryDatabase("Portfolio");
+        }, ServiceLifetime.Transient);
+
+        services.AddTransient<IEventPublisher, EventPublisher>();
+
+        services.AddTransient(typeof(IBaseRepository<,>), typeof(BaseRepository<,>))
+            .AddTransient(typeof(IBaseRepository<>), typeof(BaseRepository<>))
+            .AddTransient(typeof(ISettingService<>), typeof(SettingsService<>));
+
+        services.AddTransient<IUploadImageHelper, UploadImageHelper>()
+            .AddTransient<IMessageService, MessageService>()
+            .AddTransient<ISkillGroupService, SkillGroupService>()
+            .AddTransient<ISkillService, SkillService>()
+            .AddTransient<IAboutMeService, AboutMeService>()
+            .AddTransient<IProjectService, ProjectService>()
+            .AddTransient<IScheduleTaskService, ScheduleTaskService>()
+            .AddTransient<IUrlService, UrlService>()
+            .AddTransient<IBlogPostService, BlogPostService>()
+            .AddTransient<IBlogPostCommentService, BlogPostCommentService>()
+            .AddTransient<IBlogSubscriberService, BlogSubscriberService>()
+            .AddSingleton(new HostingConfig())
+            .AddTransient<IWebHelper, WebHelper>()
+            .AddTransient<IQueuedEmailService, QueuedEmailService>()
+            .AddTransient<IEmailService, EmailService>()
+            .AddTransient<ITokenizer, Tokenizer>()
+            .AddTransient<IMessageTokenProvider, MessageTokenProvider>()
+            .AddTransient<IPictureService, PictureService>();
+
         _serviceProvider = services.BuildServiceProvider();
+
+        EngineContext.Replace(new PortfolioTestEngine(_serviceProvider));
     }
 
     public T GetService<T>()
@@ -37,5 +104,17 @@ public abstract class BasePortfolioTest
         {
             return (T)EngineContext.Current.ResolveUnregistered(typeof(T));
         }
+    }
+
+    public partial class PortfolioTestEngine : PortfolioEngine
+    {
+        protected readonly IServiceProvider _internalServiceProvider;
+
+        public PortfolioTestEngine(IServiceProvider serviceProvider)
+        {
+            _internalServiceProvider = serviceProvider;
+        }
+
+        public override IServiceProvider ServiceProvider => _internalServiceProvider;
     }
 }
---
+++ api/Tests/Portfolio.Tests/Portfolio.Services.Tests/Blogs/BlogPostServiceTests.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using Portfolio.Domain.Models.Blogs;
+using Portfolio.Services.Blogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Tests.Portfolio.Services.Tests.Blogs;
+
+[TestFixture]
+public class BlogPostServiceTests : BasePortfolioTest
+{
+    private IBlogPostService _blogPostService;
+
+    [OneTimeSetUp]
+    public void SetUp()
+    {
+        _blogPostService = GetService<IBlogPostService>();
+
+        _blogPostService.InsertAsync(new BlogPost
+        {
+            BannerPicture = null,
+            BannerPictureId = null,
+            Comments = null,
+            Content = "Test blog 1",
+            Description = "Test blog 1",
+            CreatedAtUTC = DateTime.UtcNow,
+            DisplayNumber = 1,
+            IsPublished = true,
+            Title = "Test blog 1",
+            UpdatedAtUtc = DateTime.UtcNow,
+        }).Wait();
+
+        _blogPostService.InsertAsync(new BlogPost
+        {
+            BannerPicture = null,
+            BannerPictureId = null,
+            Comments = null,
+            Content = "Test blog 2",
+            Description = "Test blog 2",
+            CreatedAtUTC = DateTime.UtcNow,
+            DisplayNumber = 1,
+            IsPublished = false,
+            Title = "Test blog 2",
+            UpdatedAtUtc = DateTime.UtcNow,
+        }).Wait();
+    }
+
+    [Test]
+    public async Task CanGetAllPublishedBlogPosts()
+    {
+        var blogPosts = await _blogPostService.GetPublishedBlogPostsAsync();
+        blogPosts.Count().Should().Be(1);
+    }
+}
