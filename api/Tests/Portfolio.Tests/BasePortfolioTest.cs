ba15106b41a7d2e8cfa99f6a816a6a594063cd5d
Bart Blokhuis <info@bartblokhuis.com>
2022-02-01 16:03:18 +0100

    Created the new unit test library and created unit tests for caching
    Created a new unit test library using NUnit, created a base test class and created unit tests for caching. since the cache manager is inspired from the nopCommerce library I used their unit tests for testing the cache manager

---
+++ api/Tests/Portfolio.Tests/BasePortfolioTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.DependencyInjection;
+using Portfolio.Core.Caching;
+using Portfolio.Core.Infrastructure;
+using System;
+
+namespace Portfolio.Tests;
+
+public abstract class BasePortfolioTest
+{
+
+    private static readonly ServiceProvider _serviceProvider;
+
+    static BasePortfolioTest()
+    {
+        var services = new ServiceCollection();
+
+        services.AddHttpClient();
+
+        var memoryCache = new MemoryCache(new MemoryCacheOptions());
+        var typeFinder = new AppDomainTypeFinder();
+
+        services.AddSingleton<IMemoryCache>(memoryCache);
+        services.AddSingleton<IStaticCacheManager, MemoryCacheManager>();
+        services.AddSingleton<ILocker, MemoryCacheManager>();
+
+        _serviceProvider = services.BuildServiceProvider();
+    }
+
+    public T GetService<T>()
+    {
+        try
+        {
+            return _serviceProvider.GetRequiredService<T>();
+        }
+        catch (InvalidOperationException)
+        {
+            return (T)EngineContext.Current.ResolveUnregistered(typeof(T));
+        }
+    }
+}
