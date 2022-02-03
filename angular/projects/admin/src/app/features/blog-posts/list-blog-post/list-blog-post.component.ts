8acd52bbf549b1ebda2b146bb12fd48b39e679c1
Bart Blokhuis <info@bartblokhuis.com>
2022-02-03 17:14:05 +0100

    Added the data table package to the admin project

--- angular/projects/admin/src/app/features/blog-posts/list-blog-post/list-blog-post.component.ts
+++ angular/projects/admin/src/app/features/blog-posts/list-blog-post/list-blog-post.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ADTSettings } from 'angular-datatables/src/models/settings';
 import { ListBlog } from 'projects/shared/src/lib/data/blog/list-blog';
 import { Result } from 'projects/shared/src/lib/data/common/Result';
 import { BlogPostsService } from 'projects/shared/src/lib/services/api/blog-posts/blog-posts.service';
+import { Subject } from 'rxjs';
 import { BreadcrumbsService } from '../../../services/breadcrumbs/breadcrumbs.service';
 import { DeleteBlogPostComponent } from '../delete-blog-post/delete-blog-post.component';
 
@@ -15,22 +17,36 @@ import { DeleteBlogPostComponent } from '../delete-blog-post/delete-blog-post.co
 export class ListBlogPostComponent implements OnInit {
 
   blogPosts: ListBlog[] = [];
+  dtOptions = {};
+  dtTrigger: Subject<any> = new Subject<any>();
 
   constructor(private blogPostsService: BlogPostsService, private router: Router,private modalService: NgbModal, private readonly breadcrumbsService: BreadcrumbsService) { }
 
   ngOnInit(): void {
+
+    this.dtOptions = {
+      responsive: false,
+      lengthChange: false,
+      autoWidth: false,
+      searching: false,
+      orderMulti: false,
+      ordering: false,
+    }
+
     this.loadBlog();
 
     this.breadcrumbsService.setBreadcrumb([
       this.breadcrumbsService.homeCrumb,
       { name: "Blog", active: false },
       { name: `Posts`, active: true, routePath: 'blog/posts' },
-    ])
+    ]);
   }
 
   loadBlog() : void {
     this.blogPostsService.getAll().subscribe((result: Result<ListBlog[]>) => {
       this.blogPosts = result.data;
+
+      this.dtTrigger.next(this.dtOptions);
     })
   }
 
